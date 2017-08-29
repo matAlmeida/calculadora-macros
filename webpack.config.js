35a54c22eaba7851aa0afb363c0f93f3ad32f481
Matheus Almeida <mat.almeida@live.com>
2017-08-29 14:52:25 -0300

    bundle size dropped by 8 times

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
-var debug = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 var path = require('path');
+const args = require('yargs').argv;
+
+const build = args.p;
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : false,
-  entry: "./js/client.js",
+  context: path.join(__dirname, 'src'),
+  // devtool: debug ? "inline-sourcemap" : false,
+  devtool: build ? undefined : 'inline-sorcemap',
+  entry: './js/client.js',
   module: {
     loaders: [
       {
@@ -14,14 +18,18 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+          plugins: [
+            'react-html-attrs',
+            'transform-decorators-legacy',
+            'transform-class-properties',
+          ],
         }
       }
     ]
   },
   output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
+    path: __dirname + '/src/',
+    filename: 'client.min.js'
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
