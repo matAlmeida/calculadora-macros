572fda46e84d494be0d2eb3cca96081c83929033
Matheus Almeida <mat.almeida@live.com>
2017-08-31 00:29:37 -0300

    updating webpack to v3, and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-var debug = process.env.NODE_ENV !== 'production';
-var webpack = require('webpack');
-var path = require('path');
+const debug = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const path = require('path');
 const args = require('yargs').argv;
 
 const build = args.p;
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  // devtool: debug ? "inline-sourcemap" : false,
   devtool: build ? undefined : 'inline-sourcemap',
   entry: './js/client.js',
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: [
-            'react-html-attrs',
-            'transform-decorators-legacy',
-            'transform-class-properties',
-          ],
-        }
-      }
-    ]
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-0'],
+            plugins: [
+              'react-html-attrs',
+              'transform-decorators-legacy',
+              'transform-class-properties',
+            ],
+          },
+        },
+      },
+    ],
   },
   output: {
-    path: __dirname + '/src/',
+    path: path.resolve(__dirname, 'src'),
     filename: 'client.min.js'
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false }),
   ],
 };
